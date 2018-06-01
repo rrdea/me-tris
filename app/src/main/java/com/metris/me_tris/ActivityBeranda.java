08a4138ef6258d6e8879767bfb93ffe69daa0296
rrdea <deaannisayanti@gmail.com>
2018-06-01 14:04:31 +0700

    Automatic ViewPager

--- app/src/main/java/com/metris/me_tris/ActivityBeranda.java
+++ app/src/main/java/com/metris/me_tris/ActivityBeranda.java
@@ -3,11 +3,17 @@ package com.metris.me_tris;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ActivityBeranda extends AppCompatActivity {
 
+    private Timer timer;
     private ViewPager viewPager;
     private ViewPagerAdapter viewPagerAdapter;
+    private final int NUM_PAGES = 2;
 
     private String gambars[] = {
             "https://3.bp.blogspot.com/-79bvqkxoilM/WgWnsejwYCI/AAAAAAAAAgw/aEC86BU9Xd8Fax97SBSn3s2476aSoWr_gCPcBGAYYCw/s1600/d68dcd917e03da9cf85d1a8b21fa5654-compressed.jpg",
@@ -23,5 +29,30 @@ public class ActivityBeranda extends AppCompatActivity {
         viewPagerAdapter = new ViewPagerAdapter(this, gambars);
         viewPager.setAdapter(viewPagerAdapter);
 
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                viewPager.post(new Runnable(){
+
+                    @Override
+                    public void run() {
+                        if(viewPager.getCurrentItem() == NUM_PAGES - 1){
+                            viewPager.setCurrentItem(0);
+                        } else {
+                            viewPager.setCurrentItem((viewPager.getCurrentItem()+1));
+                        }
+                    }
+                });
+            }
+        };
+        timer = new Timer();
+        timer.schedule(timerTask, 3000, 3000);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        timer.cancel();
+        super.onDestroy();
     }
 }

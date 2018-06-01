442d1d843fd7d6135aec9394a2eb395881a0cd58
rrdea <deaannisayanti@gmail.com>
2018-06-01 11:55:38 +0700

    ori

---
+++ app/src/main/java/com/metris/me_tris/ActivityBeranda.java
@@ -0,0 +1,13 @@
+package com.metris.me_tris;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityBeranda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beranda);
+    }
+}
